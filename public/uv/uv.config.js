2a3da96c46284cf3fba82640a1fc2d975ed93f51
Apex-dev01 <recovery12ws@outlook.com>
2025-08-27 10:48:05 -0500

    fixed uv.config.js

--- public/uv/uv.config.js
+++ public/uv/uv.config.js
@@ -1,6 +1,6 @@
 self.__uv$config = {
     prefix: '/service/',
-    bare: 'https://apex-pv.vercel.app//bare/',
+    bare: 'https://apex-pv.vercel.app/bare/',
     encodeUrl: Ultraviolet.codec.xor.encode,
     decodeUrl: Ultraviolet.codec.xor.decode,
     handler: '/uv/uv.handler.js',
